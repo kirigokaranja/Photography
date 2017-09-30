71261586dfff1187f57e75364c799e3898970d70
kirigo karanja <sharonkirigo@gmail.com>
2017-09-30 08:27:41 +0300

    new changes done

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Login Page for Foto ">
+    <meta name="author" content=" Foto">
+    <link rel="icon" type="image/ico" href="favicon.ico" />
+
+    <title>Login</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+<form class="loginform">
+    <fieldset class="loginfield">
+        <h1> Foto</h1>
+        <img src="mail.png"><input type="email" name="email" title="email"  ><br><br>
+        <input type="password" title="password" name="password"><br><br>
+        <button type="submit">Log in</button><br>
+    </fieldset>
+</form>
+</body>
+</html>
