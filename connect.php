85cf57a300f8f8c38b32ba02786348b1737ea7da
kirigo karanja <sharonkirigo@gmail.com>
2017-10-04 10:40:48 +0300

    signin completed and book form done

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php	
+	$db=mysqli_connect("localhost", "root","") ;
+	if (!$db) {
+        die ("Could not connect to database" . mysqli_connect_error());
+    }
+
+	mysqli_select_db($db,'foto') or die('Error selecting database : ' . mysql_error());
+
+
+
+
