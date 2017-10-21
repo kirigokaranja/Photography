50a1cda201b6455fcbbd9a2909d2bce6f7d2afd7
GithinjiWanjohi <gyth.wanjohi@gmail.com>
2017-10-21 21:39:52 +0300

    Image dropzone

--- upload.php
+++ upload.php
@@ -6,13 +6,37 @@
  * Time: 9:39 PM
  */
 
-if(!empty($_FILES)){
-    $temp = $_FILES['file']['tmp_name'];
-    $dir_separator=DIRECTORY_SEPARATOR;
-    $folder="images";
+session_start();
+include ('connect.php');
+$id = $_SESSION['email'];
 
-    $destination_path = dirname(__FILE__).$dir_separator.$folder.$dir_separator;
+$s = "SELECT * FROM customer WHERE email = '$id'";
+global $db;
+$res = $db->query($s) or trigger_error($db->error . "[$s]");
 
-    $target_path = $destination_path.$_FILES['file']['name'];
-    move_uploaded_file($temp, $target_path);
+while ($row = mysqli_fetch_array($res)) {
+    $name = $row['name'];
+    $custId = $row['custID'];
+
+    $destination_path = dirname(__FILE__) .$userFolder;
+
+    if (!empty($_FILES)) {
+        $temp = $_FILES['file']['tmp_name'];
+        $dir_separator = DIRECTORY_SEPARATOR;
+        $folder = "images";
+        $id_separator = "-";
+        $userFolder =$dir_separator. $folder . $dir_separator.$name.$id_separator.$custId . $dir_separator;
+
+        $destination_path = dirname(__FILE__) .$userFolder;
+
+        $target_path = $destination_path . $_FILES['file']['name'];
+        if ($_FILES["fileToUpload"]["size"] > 10000000) {
+            echo "Sorry, your file is too large.";
+        } else if (!is_dir($destination_path)) {
+            mkdir($destination_path);
+            move_uploaded_file($temp, $target_path);
+        } else {
+            move_uploaded_file($temp, $target_path);
+        }
+    }
 }
