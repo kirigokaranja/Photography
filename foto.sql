ea5c3f96d21b50d39263777440eb0b6a320590a4
GithinjiWanjohi <gyth.wanjohi@gmail.com>
2017-10-25 15:00:49 +0300

    Gallery for users

--- foto.sql
+++ foto.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 04, 2017 at 09:43 AM
+-- Generation Time: Oct 25, 2017 at 01:55 PM
 -- Server version: 10.1.19-MariaDB
--- PHP Version: 7.0.13
+-- PHP Version: 5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -44,7 +44,8 @@ CREATE TABLE `book` (
 
 CREATE TABLE `customer` (
   `custID` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `phone_number` int(11) NOT NULL,
   `picture` varchar(255) NOT NULL
@@ -54,8 +55,13 @@ CREATE TABLE `customer` (
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`custID`, `name`, `email`, `phone_number`, `picture`) VALUES
-(7, 'kirigo karanja', 'sharon@gmail.com', 713774575, '');
+INSERT INTO `customer` (`custID`, `firstName`, `surname`, `email`, `phone_number`, `picture`) VALUES
+(7, 'kirigo karanja', '', 'sharon@gmail.com', 713774575, ''),
+(8, 'Githinji Wanjohi', '', 'gyth.wanjohi@gmail.com', 710700123, ''),
+(9, 'g', '', 'g@g.com', 724536725, ''),
+(10, 'Sheldon', 'Cooper', 'sheldon@gmail.com', 717472552, ''),
+(11, 'Mkenya', 'Daima', 'mk@gmail.com', 717643735, ''),
+(12, 'Wesley', 'Jabuya', 'jabuya@gmail.com', 714363543, '');
 
 -- --------------------------------------------------------
 
@@ -83,6 +89,14 @@ CREATE TABLE `messages` (
   `email` varchar(255) NOT NULL,
   `message` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`messID`, `name`, `email`, `message`) VALUES
+(1, 'Githinji Wanjohi', 'gyth.wanjohi@gmail.com', 'I want photos!!'),
+(2, '', '', '');
 
 -- --------------------------------------------------------
 
@@ -128,7 +142,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_type`, `email`, `password`) VALUES
-('customer', 'sharon@gmail.com', '1234');
+('customer', 'g@g.com', '123'),
+('customer', 'gyth.wanjohi@gmail.com', '1234'),
+('customer', 'jabuya@gmail.com', '1234'),
+('customer', 'mk@gmail.com', '1234'),
+('customer', 'sharon@gmail.com', '1234'),
+('customer', 'sheldon@gmail.com', '1234');
 
 --
 -- Indexes for dumped tables
@@ -195,7 +214,7 @@ ALTER TABLE `book`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `custID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `custID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `customer_upload`
 --
@@ -205,7 +224,7 @@ ALTER TABLE `customer_upload`
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `messID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `messID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `photographer`
 --
