c8fb974a89c96478a0eca4d01d3c1150d7834377
kirigo karanja <sharonkirigo@gmail.com>
2017-11-07 11:04:16 +0300

    add genre

---
+++ main/genre.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="">
+<!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/ico" href="../favicon.ico" />
+    <title>MainPhotographer | Genre</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/flexslider.css">
+    <link rel="stylesheet" href="../css/jquery.fancybox.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/responsive.css">
+    <link rel="stylesheet" href="../css/animate.min.css">
+    <link rel="stylesheet" href="../css/font-icon.css">
+    <link rel="stylesheet" href="../css/menuDropdown.css">
+    <link rel="stylesheet" href="../css/dropzone.css">
+    <link rel="stylesheet" href="css/upload.css">
+    <link rel="stylesheet" href="../css/gallery.css">
+    <link rel="stylesheet" href="css/genre.css">
+    <script src="../dist/sweetalert.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../dist/sweetalert.css">
+</head>
+<body>
+<?php
+session_start();
+include ('connect.php');
+if(isset($_SESSION['Admin'])) {
+    ?>
+
+    <section class="hero" role="banner">
+
+        <!-- banner text -->
+        <div class="container">
+            <br><br>
+            <div class="header-content clearfix"> <a class="logo" href="#"><img src="images/logo.ai" alt=""></a>
+                <nav class="navigation" role="navigation">
+                    <ul class="primary-nav">
+                        <li><a href="dashboard.php">Dashboard</a></li>
+                        <li><a href="dashboard.php">User Uploads</a></li>
+                        <li><a href="message.php">Messages</a></li>
+                        <li><a href="photographer.php">Photographers</a></li>
+                        <li><a href="addPictures.php">Bookings</a></li>
+                        <li><a href="genre.php">Genre</a></li>
+                        <li><a href="logout.php" class="btn btn-large">Logout</a></li>
+
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-md-10 col-md-offset-1">
+
+                <div class="hero-text text-center">
+                    <h1>Main Photographer</h1><br>
+                    <p style="color: #003434;">Genre</p>
+
+                </div>
+                <!-- banner text -->
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container1">
+            <div class="picture-holder"></div>
+            <div class="story-holder">
+                <h1 class="formh1">Genre</h1><br><br><br>
+                <form method="post" action="genre_action.php">
+                    <label>
+                        <span>Genre Name</span><br>
+                        <input type="text" name="genre" required/>
+                    </label><br><br>
+                    <div class="text-center">
+                        <button type="submit" class="buttonsubmit">Add Genre</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+
+<?php
+}else{
+
+
+?>
+    <script>
+
+        swal({
+                title: "Login Required!",
+                text: "Please login to access genre",
+                type: "info",
+                showCancelButton: true,
+                confirmButtonClass: "btn-danger",
+                confirmButtonText: "Login",
+                cancelButtonText: "Cancel",
+                closeOnConfirm: false,
+                closeOnCancel: false
+            },
+            function (isConfirm) {
+                if (isConfirm) {
+                    location.href = "../log.php"
+                } else {
+                    location.href = "../index.php"
+                }
+            });
+    </script>
+<?php } ?>
+
+</body>
+</html>
