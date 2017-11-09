878c8022baf61a8c548e7bc3455501c9be46f110
GithinjiWanjohi <gyth.wanjohi@gmail.com>
2017-11-09 11:07:37 +0300

    Configured db for changes in the photographer module.

---
+++ test.php
@@ -0,0 +1,53 @@
+<?php
+include ('connect.php');
+session_start();
+
+$folder1 = $_GET['folderName'];
+$folderDir = $_GET['folderDir'];
+$userID = $_GET['userID'];
+$dir = "images";
+$zip_file = $folderDir."/".$folder1.'.zip';
+
+// Get real path for our folder
+$rootPath = realpath($folderDir);
+
+global $db;
+$status = "In Progress";
+$sql = "UPDATE customer_upload SET edit_status='$status'
+          WHERE custID='$userID' AND folderName='$folder1'";
+$res = $db->query($sql) or trigger_error($db->error."[$sql]");
+
+// Initialize archive object
+$zip = new ZipArchive();
+$zip->open($zip_file, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+
+// Create recursive directory iterator
+/** @var SplFileInfo[] $files */
+$files = new RecursiveIteratorIterator(
+    new RecursiveDirectoryIterator($rootPath),
+    RecursiveIteratorIterator::LEAVES_ONLY
+);
+
+foreach ($files as $name => $file)
+{
+    // Skip directories (they would be added automatically)
+    if (!$file->isDir())
+    {
+        // Get real and relative path for current file
+        $filePath = $file->getRealPath();
+        $relativePath = substr($filePath, strlen($rootPath) + 1);
+
+        // Add current file to archive
+        $zip->addFile($filePath, $relativePath);
+    }
+}
+
+
+header("Content-type: application/zip");
+header("Content-Disposition: attachment; filename=$zip_file");
+header("Content-length: " . filesize($zip_file));
+header("Pragma: no-cache");
+header("Expires: 0");
+readfile("$zip_file");
+
+header("Refresh:0; url=../main/dashboard2.php");
